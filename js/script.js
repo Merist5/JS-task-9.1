cf2b46552809f858454f10d906568d80a8c3d7d5
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-04-29 15:34:05 +0200

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function getTriangleArea (a, h) {
+  if ( (a <= 0) || (h <= 0) ) {
+    console.log('Nieprawidłowe dane');
+  } else if ( (a >= 0) && (h >= 0) ) {
+    return a * h / 2;
+  }
+}
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(0, 8);
+var triangle3Area = getTriangleArea(7, 12);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
